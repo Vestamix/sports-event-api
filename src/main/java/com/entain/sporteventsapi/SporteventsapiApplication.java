f6d64e5ec8328d8b1b0879c299f61dc58923b59f
Alex M <aleksandrsmusaluks@gmail.com>
2024-11-23 11:58:18 +0200

    Application main and properties added

---
+++ src/main/java/com/entain/sporteventsapi/SporteventsapiApplication.java
@@ -0,0 +1,13 @@
+package com.entain.sporteventsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporteventsapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SporteventsapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/entain/sporteventsapi/SporteventsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entain.sporteventsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SporteventsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
